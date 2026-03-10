31713b78bf8e6d7ee858347d01e3255dfd846352
Your Name <you@example.com>
2026-03-10 17:20:40 +0800

    debug: Add red background to button area for cache busting test

--- src/components/gallery/GalleryCard.tsx
+++ src/components/gallery/GalleryCard.tsx
@@ -62,7 +62,7 @@ const GalleryCard = ({ palette }: GalleryCardProps) => {
         </div>
       </div>
       
-      <div className="absolute -top-5 -right-5 flex flex-col gap-2">
+      <div className="absolute -top-5 -right-5 flex flex-col gap-2 bg-red-500 p-2 rounded-lg">
         {!generatedImage ? (
           <button 
             onClick={generateImage}
